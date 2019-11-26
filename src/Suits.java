da4c3b354e7b2eddbd0927f27ea427837cef72a0
hundredvisionsguy <cwinikka@gmail.com>
2019-11-25 18:02:16 -0800

    adding TODO notes to Card, Ranks, and Suits

--- src/Suits.java
+++ src/Suits.java
@@ -1,4 +1,12 @@
 public enum Suits {
+  /* 
+    TODO: using the context, replace all blanks with appropriate code
+    
+    NOTE: you may wish to reference the following article:
+    Java Enums: How to Make Enums More Useful
+    https://dzone.com/articles/java-enums-how-to-make-much-more-useful 
+  */
+  
   SPADES("Spades"), 
   ____("Hearts"), 
   CLUBS(____), 
